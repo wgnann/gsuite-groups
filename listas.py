c1832be6b46d712eb41990307320a1187662da91
Will <wgnann@gmail.com>
2019-06-07 18:54:57 -0300

    Refatorando para usar OO

--- listas.py
+++ listas.py
@@ -7,51 +7,66 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 
+class GoogleGroup:
+    def __init__(self, grupo, dominio):
+        self.grupo = grupo
+        self.dominio = dominio
+        login = config('LOGIN')
+        password = config('PASSWORD')
+        url = 'https://groups.google.com/a/'+self.dominio+'/forum/#!managemembers/'+self.grupo+'/add'
+
+        # browser
+        browser = webdriver.Firefox()
+        browser.get(url)
+        wait = WebDriverWait(browser, 30)
+
+        # google login
+        element = wait.until(ec.presence_of_element_located((By.ID, "identifierId")))
+        element.send_keys(login)
+        element.send_keys(Keys.ENTER)
+
+        # idpcafe login
+        element = wait.until(ec.presence_of_element_located((By.ID, "username")))
+        element.send_keys(login)
+        element = wait.until(ec.presence_of_element_located((By.ID, "password")))
+        element.send_keys(password)
+        element.send_keys(Keys.ENTER)
+
+        self.browser = browser
+        self.wait = wait
+
+    def subscribe(self, emails):
+        browser = self.browser
+        wait = self.wait
+
+        # groups interface
+        element = wait.until(ec.presence_of_element_located((By.CLASS_NAME, "gwt-TextArea")))
+        for email in emails:
+            element.send_keys(email)
+            element.send_keys(Keys.ENTER)
+        element = wait.until(ec.presence_of_element_located((By.CLASS_NAME, "jfk-button-action")))
+        element.click()
+
+        # espera carregar e fecha
+        element = wait.until(ec.presence_of_element_located((By.CLASS_NAME, "F0XO1GC-Nb-e")))
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("lista", help="endereço da lista de email")
     parser.add_argument("emails", nargs='?', help="arquivo de emails")
     args = parser.parse_args()
 
-    login = config('LOGIN')
-    password = config('PASSWORD')
     grupo, dominio = args.lista.split('@')
     mailfile = args.emails
-    url = 'https://groups.google.com/a/'+dominio+'/forum/#!managemembers/'+grupo+'/add'
 
     if (args.emails):
         emails = open(mailfile, 'r').read().splitlines()
     else:
         emails = [line.rstrip() for line in sys.stdin]
 
-    # browser
-    browser = webdriver.Firefox()
-    browser.get(url)
-    wait = WebDriverWait(browser, 30)
-
-    # google login
-    element = wait.until(ec.presence_of_element_located((By.ID, "identifierId")))
-    element.send_keys(login)
-    element.send_keys(Keys.ENTER)
-
-    # idpcafe login
-    element = wait.until(ec.presence_of_element_located((By.ID, "username")))
-    element.send_keys(login)
-    element = wait.until(ec.presence_of_element_located((By.ID, "password")))
-    element.send_keys(password)
-    element.send_keys(Keys.ENTER)
-
-    # groups interface
-    element = wait.until(ec.presence_of_element_located((By.CLASS_NAME, "gwt-TextArea")))
-    for email in emails:
-        element.send_keys(email)
-        element.send_keys(Keys.ENTER)
-    element = wait.until(ec.presence_of_element_located((By.CLASS_NAME, "jfk-button-action")))
-    element.click()
-
-    # espera carregar e fecha
-    element = wait.until(ec.presence_of_element_located((By.CLASS_NAME, "F0XO1GC-Nb-e")))
-    browser.close()
+    google = GoogleGroup(grupo, dominio)
+    google.subscribe(emails)
+    google.browser.close()
 
 if __name__ == "__main__":
     main()
